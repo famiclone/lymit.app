52751ab4226789bd9fd590283486b0e45b5ccc87
Denys Tynok <denys@famiclone.dev>
2023-11-02 00:04:05 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { MdAccountBalanceWallet, MdSettings } from 'react-icons/md';
+import settingsIcon from './assets/icons/settings.svg';
 import './App.css';
 
+type AppState = {
+  dayLimit: number;
+  currentDate: Date;
+  total: number;
+  currency: string;
+  nextPaymentDate: Date;
+}
+
 function App() {
+  const [state, setState] = useState<AppState>({
+    dayLimit: 0,
+    total: 5868,
+    currentDate: new Date(),
+    currency: 'UAH',
+    nextPaymentDate: new Date(2023, 10, 15 + 1),
+  });
+
+  function getDaysLeft(): number {
+    const { currentDate, nextPaymentDate } = state;
+    const daysLeft = Math.floor((nextPaymentDate.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
+    return daysLeft;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__content">
+        <header className="app__header">
+          <h1 className="app__header-title">Balance</h1>
+        </header>
+        <div className="card">
+          <div className="card__title primary">Daily limit</div>
+          <div className="d-flex">
+            <div className="text--big daylimit">{(state.total / getDaysLeft()).toFixed(2)}</div>
+            <div className="text--secondary">{state.currency}</div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__title secondary">Total amount</div>
+          <div className="d-flex">
+            <div className="daylimit">{(state.total)}</div>
+            <div className="text--secondary">{state.currency}</div>
+          </div>
+        </div>
+      </div>
+
+      <footer className="app__footer">
+        <div className="footer__button active">
+          <MdAccountBalanceWallet size={32}/>
+          Balance
+        </div>
+        <div className="footer__button">
+          <MdSettings size={32}/>
+          Settings
+        </div>
+      </footer>
     </div>
   );
 }
